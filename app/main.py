1a686b6a3eb7093c40dc3726f796e555cfca84a4
Franco Canzani <francocanzani@gmail.com>
2024-06-29 19:03:27 +0200

    adds cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.routers import youtube
+from fastapi_cors import CORS
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORS,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(youtube.router, prefix="/youtube", tags=["youtube"])
 
